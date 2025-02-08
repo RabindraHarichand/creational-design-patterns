fcc091bc88f3ca8676069d958cc517a460bca15b
Rabindra Harichand <rharichand.17@est.ucab.edu.ve>
2025-02-08 18:55:53 -0400

    [Builder]: Implement Builder pattern for Computer configuration with display functionality

--- 01-creacionales/01-builder.ts
+++ 01-creacionales/01-builder.ts
@@ -12,3 +12,77 @@
  *
  * https://refactoring.guru/es/design-patterns/builder
  */
+
+import { COLORS } from "../helpers/colors.ts";
+
+class Computer {
+  public cpu: string = "cpu - not defined";
+  public ram: string = "ram - not defined";
+  public storage: string = "storage - not defined";
+  public gpu?: string;
+
+  displayConfiguration() {
+    console.log(`Configuracion de la computadora 
+        CPU: ${this.cpu}
+        RAM: ${this.ram}
+        Almamcenamiento: ${this.storage}
+        GPU: ${this.gpu ?? "No tiene GPU"}
+        `);
+  }
+}
+
+class ComputerBuilder {
+  private computer: Computer;
+
+  constructor() {
+    this.computer = new Computer();
+  }
+
+  setCPU(cpu: string): ComputerBuilder {
+    this.computer.cpu = cpu;
+
+    return this;
+  }
+  setRAM(ram: string): ComputerBuilder {
+    this.computer.ram = ram;
+
+    return this;
+  }
+  setStorage(storage: string): ComputerBuilder {
+    this.computer.storage = storage;
+
+    return this;
+  }
+  setGPU(gpu: string): ComputerBuilder {
+    this.computer.gpu = gpu;
+
+    return this;
+  }
+
+  build() {
+    return this.computer;
+  }
+}
+
+function main() {
+  const basicComputer: Computer = new ComputerBuilder()
+    .setCPU("Intel Core 2 Duo")
+    .setRAM("4 GB")
+    .setStorage("256GB")
+    .build();
+
+  console.log("%cComputadora basica:", COLORS.blue);
+  basicComputer.displayConfiguration();
+
+  const Legion5000: Computer = new ComputerBuilder()
+    .setCPU("Intel Core I9 H")
+    .setRAM("65 GB")
+    .setStorage("2 TB")
+    .setGPU("Nvidia 5000")
+    .build();
+
+  console.log("%cComputadora elite:", COLORS.red);
+  Legion5000.displayConfiguration();
+}
+
+main();
